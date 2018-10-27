606d0fa2025c1e7cf351149ec06b634cf7cc0ea3
Hossein Panahi <h.panahi.ird@gmail.com>
2018-10-27 14:03:53 +0330

    created a server that listens to connections and then accepts them.

---
+++ Proxy.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+
+def proxy():
+    host = '127.0.0.1'
+    port = 5000
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("socket created")
+
+    try:
+        server.bind((host,port))
+    except socket.error as msg:
+        print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+    print("server bind complete")
+
+    server.listen(10)
+    print("server is now listening")
+
+    while 1:
+        conn, addr=server.accept()
+        print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+
+if __name__=="__main__":
+    proxy()

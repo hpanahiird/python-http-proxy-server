a79e254a172947925d370cea33056b2e6581a837
Hossein Panahi <h.panahi.ird@gmail.com>
2018-10-28 17:41:28 +0330

    added multi thread to server and reading request.

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 
 def server():
@@ -12,6 +13,29 @@ def server():
         conn, addr = server.accept()
         print("new connection")
         print(addr)
+        handlerThread = threading.Thread(target=clientHandler, args=(conn,))
+        handlerThread.start()
+
+
+def clientHandler(client):
+    buffer_size = 128
+    buffer = client.recv(buffer_size)
+    client_request = buffer
+    while len(buffer) == buffer_size:
+        buffer = client.recv(buffer_size)
+        client_request += buffer
+    # tmp = client_request[client_request.find(b'Host'):]
+    # tmp = tmp[len('host: '):tmp.find(b'\n')]
+    # host = tmp[:-1].decode("utf-8")
+
+    print(client_request)
+
+    # path = client_request.split()[1]
+    # print(path)
+    # params = path[path.find(b'?') + 1:]
+    # print(params)
+    # print(params.split(b'&'))
+    # print(host)
 
 
 if __name__=="__main__":
